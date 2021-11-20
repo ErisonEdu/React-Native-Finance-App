d8bc5a437e31195ab6e2c750308e16ae090a51f0
Erison Pimentao <erison.pimentao@gmail.com>
2021-11-20 07:44:13 -0300

    Header Component

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 
-import { Container, Header } from './styles';
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+} from './styles';
 
 export function Dashboard() {
     return(
         <>
             <Container>
                 <Header>
-                    Olá React Native! XD
+                    <UserWrapper>
+                        <UserInfo>
+                            <Photo source={{uri:'https://avatars.githubusercontent.com/u/25579326?v=4'}}/>
+                            
+                            <User>
+                                <UserGreeting>Nice To See U Again,</UserGreeting>
+                                <UserName>Erison</UserName>
+                            </User>
+                        </UserInfo>
+                    </UserWrapper>
                 </Header>
             </Container>
         </>
