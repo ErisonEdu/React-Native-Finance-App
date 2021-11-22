aedfe7381efc76c635f28d67fdcdaf89c03df607
Erison Pimentao <erison.pimentao@gmail.com>
2021-11-22 06:24:01 -0300

    color name has been fixed and added new colors

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,6 +1,7 @@
 export default {
     colors: {
         primary:'#5636D3',
+        primary_light: 'rgba(86, 54, 211, .5)',
 
         secondary: '#FF872C',
         secondary_light:'rgba(255, 135, 44,.3)',
@@ -9,8 +10,9 @@ export default {
         success_light: 'rgba(18, 164, 84, .5)',
         
         attention: '#E83F5B',
-        attencion_light: 'rgba(232, 63, 91, .5)',
+        attention_light: 'rgba(232, 63, 91, .5)',
 
+        golden: ' #FFD700',
         shape: '#FFFFFF',
         title:'#363F5F',
         text: '#969CB2',
